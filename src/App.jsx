bf9dd34326dda915fe23c29804b45481055c6960
monognuisy <ysmsmart1@gmail.com>
2023-11-15 16:33:18 +0900

    feat: group balloons with union-find

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import styled from 'styled-components';
+import './App.css';
+import Grid from './components/Grid';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [height, setHeight] = useState(5);
+  const [width, setWidth] = useState(5);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <input
+        placeholder="height"
+        value={height}
+        onChange={(e) => setHeight(e.target.value)}
+      />
+      <input
+        placeholder="width"
+        value={width}
+        onChange={(e) => setWidth(e.target.value)}
+      />
+      {/* <Grid height={height} width={width} /> */}
+      <Grid height={5} width={5} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Grid.jsx
@@ -0,0 +1,132 @@
+import { useEffect, useState } from 'react';
+import styled, { ThemeProvider } from 'styled-components';
+
+const Blank = () => {
+  return <BlankDiv></BlankDiv>;
+};
+
+const Balloon = ({ num }) => {
+  return (
+    <BalloonDiv>
+      <p>{num}</p>
+    </BalloonDiv>
+  );
+};
+
+const Grid = ({ height, width }) => {
+  const [realMap, setRealMap] = useState([]);
+  const [balloons, setBalloons] = useState({});
+
+  const find = (x, parent) => {
+    if (parent[x] === x) return x;
+
+    const currentParent = find(parent[x], parent);
+    parent[x] = currentParent;
+
+    return currentParent;
+  };
+
+  const union = (a, b, parent) => {
+    const rootA = find(a, parent);
+    const rootB = find(b, parent);
+
+    if (rootA < rootB) parent[rootB] = rootA;
+    else parent[rootA] = rootB;
+  };
+
+  const unionFind = (iRange, jRange, balloonMap, parent) => {
+    iRange.forEach((i) => {
+      jRange.forEach((j) => {
+        if (!balloonMap[i][j]) return;
+
+        if (balloonMap[i - 1][j]) {
+          union(balloonMap[i][j], balloonMap[i - 1][j], parent);
+        }
+        if (balloonMap[i][j - 1]) {
+          union(balloonMap[i][j], balloonMap[i][j - 1], parent);
+        }
+        if (balloonMap[i + 1][j]) {
+          union(balloonMap[i + 1][j], balloonMap[i + 1][j], parent);
+        }
+        if (balloonMap[i - 1][j]) {
+          union(balloonMap[i][j + 1], balloonMap[i][j + 1], parent);
+        }
+
+        balloonMap[i][j] = parent[balloonMap[i][j]];
+      });
+    });
+  };
+
+  // create Balloon, Blank map
+  useEffect(() => {
+    const balloonMap = [
+      [...Array(width + 2).fill(0)],
+      ...Array(height)
+        .fill(0)
+        .map(() => [...Array(width + 2).fill(0)]),
+      [...Array(width + 2).fill(0)],
+    ];
+
+    const parent = [0];
+
+    let k = 1;
+    for (let i = 1; i <= height; i++) {
+      for (let j = 1; j <= width; j++) {
+        const isBalloon = Math.random() > 0.5;
+
+        if (!isBalloon) {
+          balloonMap[i][j] = 0;
+          continue;
+        }
+
+        balloonMap[i][j] = k;
+        parent.push(k);
+        k++;
+      }
+    }
+
+    const iRange = [...Array(height).keys()].map((i) => i + 1);
+    const jRange = [...Array(width).keys()].map((i) => i + 1);
+
+    unionFind(iRange, jRange, balloonMap, parent);
+    unionFind(iRange, [...jRange].reverse(), balloonMap, parent);
+    unionFind([...iRange].reverse(), jRange, balloonMap, parent);
+    unionFind([...iRange].reverse(), [...jRange].reverse(), balloonMap, parent);
+
+    setRealMap(
+      balloonMap.slice(1, height + 1).map((a) => a.slice(1, width + 1)),
+    );
+  }, []);
+
+  const cells = []
+    .concat(...realMap)
+    .map((n) => (n ? <Balloon num={n} /> : <Blank />));
+
+  return (
+    <>
+      <GridMain height={height} width={width}>
+        {cells}
+      </GridMain>
+    </>
+  );
+};
+
+const BlankDiv = styled.div`
+  width: 100%;
+  height: 100%;
+  border: 1px solid;
+`;
+const BalloonDiv = styled.div`
+  width: 100%;
+  height: 100%;
+  border: 1px solid;
+`;
+const GridMain = styled.div`
+  display: grid;
+  width: 500px;
+  height: 500px;
+  grid-template-columns: repeat(${(props) => props.width}, 1fr);
+  grid-template-rows: repeat(${(props) => props.height}, 1fr);
+`;
+
+export default Grid;
